2f34965efe711ba452eef7620068969ff9dc59aa
Lev Lamberov <dogsleg@debian.org>
2018-02-08 16:05:10 +0500

    Add a boilerplate of our module

---
+++ YSTR.pm
@@ -0,0 +1,41 @@
+package YSTR;
+use strict;
+use warnings;
+
+sub new {
+    return
+}
+
+sub get_minimal {
+    return
+}
+
+sub get_powerplexy {
+    return
+}
+
+sub get_yfiler {
+    return
+}
+
+sub get_powerplexy23 {
+    return
+}
+
+sub get_yfilerplus {
+    return
+}
+
+sub get_maximal {
+    return
+}
+
+sub get_ftdna {
+    return
+}
+
+sub get_yfull {
+    return
+}
+
+1;
